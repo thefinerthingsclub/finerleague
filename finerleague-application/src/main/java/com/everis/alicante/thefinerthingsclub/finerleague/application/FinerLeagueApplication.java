da7d69496f971d88fe86c5a37f5147c4d94e5652
Enrique Mingorance Cano <emingora@everis.com>
2016-12-20 17:09:13 +0100

    Very First Commit

---
+++ finerleague-application/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/application/FinerLeagueApplication.java
@@ -0,0 +1,59 @@
+package com.everis.alicante.thefinerthingsclub.finerleague.application;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * The Class FinerLeagueApplication.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.everis.alicante.thefinerthingsclub.finerleague" })
+public class FinerLeagueApplication extends SpringBootServletInitializer {
+
+	/**
+	 * The Logger.
+	 */
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * The Application class.
+	 */
+	private static Class<FinerLeagueApplication> applicationClass = FinerLeagueApplication.class;
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args
+	 *            the input arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(FinerLeagueApplication.class, args);
+	}
+
+	/**
+	 * Init.
+	 */
+	@PostConstruct
+	public void init() {
+		logger.info("Initializing FinerApplication...");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.boot.context.web.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+}
