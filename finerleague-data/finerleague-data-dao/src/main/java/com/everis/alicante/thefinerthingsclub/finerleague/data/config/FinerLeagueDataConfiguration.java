f33b45fac3e6bd4935b4cceeb070501d6f61d15c
Enrique Mingorance Cano <emingora@everis.com>
2016-12-23 09:43:25 +0100

    FinerLeagueDataConfiguration created. Spring version set

---
+++ finerleague-data/finerleague-data-dao/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/config/FinerLeagueDataConfiguration.java
@@ -0,0 +1,13 @@
+package com.everis.alicante.thefinerthingsclub.finerleague.data.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The Class the setup the data layer
+ */
+@Configuration
+public class FinerLeagueDataConfiguration {
+
+}
+
+
