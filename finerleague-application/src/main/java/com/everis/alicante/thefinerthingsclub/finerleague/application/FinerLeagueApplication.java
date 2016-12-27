1f4577983fc67880392945807d9d07bc6a1d3cb3
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2016-12-27 15:16:35 +0100

    MongoDB setup. Embeded MongoDB

--- finerleague-application/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/application/FinerLeagueApplication.java
+++ finerleague-application/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/application/FinerLeagueApplication.java
@@ -18,44 +18,45 @@ import javax.annotation.PostConstruct;
  * The Class FinerLeagueApplication.
  */
 @Configuration
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@ComponentScan(basePackages = { "com.everis.alicante.thefinerthingsclub.finerleague" })
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.everis.alicante.thefinerthingsclub.finerleague"})
 public class FinerLeagueApplication extends SpringBootServletInitializer {
 
-	/**
-	 * The Logger.
-	 */
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    /**
+     * The Logger.
+     */
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	/**
-	 * The Application class.
-	 */
-	private static Class<FinerLeagueApplication> applicationClass = FinerLeagueApplication.class;
+    /**
+     * The Application class.
+     */
+    private static Class<FinerLeagueApplication> applicationClass = FinerLeagueApplication.class;
 
-	/**
-	 * The entry point of application.
-	 *
-	 * @param args
-	 *            the input arguments
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(FinerLeagueApplication.class, args);
-	}
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        // TODO
+        System.setProperty("http.proxyHost", "10.0.8.102");
+        System.setProperty("http.proxyPort", "8080");
+        SpringApplication.run(FinerLeagueApplication.class, args);
+    }
 
-	/**
-	 * Init.
-	 */
-	@PostConstruct
-	public void init() {
-		logger.info("Initializing FinerLeague Application...");
-	}
+    /**
+     * Init.
+     */
+    @PostConstruct
+    public void init() {
+        logger.info("Initializing FinerLeague Application...");
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.boot.context.web.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
-	 */
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
-	}
-
+    /* (non-Javadoc)
+     * @see org.springframework.boot.context.web.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 }
