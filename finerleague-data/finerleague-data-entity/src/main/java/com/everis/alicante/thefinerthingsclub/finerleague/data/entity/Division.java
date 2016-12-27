79697b936886510189d499ebc6b04b5a2dbba511
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2016-12-27 17:18:28 +0100

    spring-data-mongodb dependency added to data-entity

--- finerleague-data/finerleague-data-entity/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/entity/Division.java
+++ finerleague-data/finerleague-data-entity/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/entity/Division.java
@@ -1,6 +1,9 @@
 package com.everis.alicante.thefinerthingsclub.finerleague.data.entity;
 
+import org.springframework.data.annotation.Id;
+
 public class Division {
 
+    @Id
     private String id;
 }
