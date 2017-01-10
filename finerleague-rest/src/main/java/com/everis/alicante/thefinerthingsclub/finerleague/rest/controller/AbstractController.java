44f3b387b41cdf4a6b0356e217eff49f5037cab3
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2017-01-10 16:39:15 +0100

    imports reordering

--- finerleague-rest/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/rest/controller/AbstractController.java
+++ finerleague-rest/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/rest/controller/AbstractController.java
@@ -7,10 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
