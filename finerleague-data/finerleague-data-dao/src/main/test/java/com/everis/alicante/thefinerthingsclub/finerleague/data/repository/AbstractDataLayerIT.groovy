bcc41639de01f36857c0122ffdead1e178915219
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2017-01-10 09:12:15 +0100

    DataLayer IT ready

--- finerleague-data/finerleague-data-dao/src/main/test/java/com/everis/alicante/thefinerthingsclub/finerleague/data/repository/AbstractDataLayerIT.groovy
+++ finerleague-data/finerleague-data-dao/src/main/test/java/com/everis/alicante/thefinerthingsclub/finerleague/data/repository/AbstractDataLayerIT.groovy
@@ -1,7 +1,11 @@
 package com.everis.alicante.thefinerthingsclub.finerleague.data.repository
 
 import com.everis.alicante.thefinerthingsclub.finerleague.data.entity.EntityDocument
+import org.apache.commons.io.FileUtils
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ResourceLoader
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
@@ -26,11 +30,25 @@ abstract class AbstractDataLayerIT<E extends EntityDocument, ID extends Serializ
     abstract R getRepository();
 
     /**
+     * The Mongo Template
+     */
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    /**
+     * The Spring resource loader
+     */
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    /**
      * IT Setup
      *
      */
     def setup() {
         getRepository().deleteAll();
+        def resource = resourceLoader.getResource("classpath:datasets/initialdata.json");
+        mongoTemplate.save(FileUtils.readFileToString(resource.getFile()));
     }
 
     /**
