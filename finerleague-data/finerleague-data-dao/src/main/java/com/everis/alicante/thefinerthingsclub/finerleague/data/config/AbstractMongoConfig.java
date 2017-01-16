84a1f65facc50ad892c5c9887585c5e33bd1e27f
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2017-01-16 12:22:00 +0100

    Mongeez added. Initial changeset loaded

--- finerleague-data/finerleague-data-dao/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/config/AbstractMongoConfig.java
+++ finerleague-data/finerleague-data-dao/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/config/AbstractMongoConfig.java
@@ -1,9 +1,12 @@
 package com.everis.alicante.thefinerthingsclub.finerleague.data.config;
 
 import com.everis.alicante.thefinerthingsclub.finerleague.common.domain.constants.FinerLeagueConfigurationConstants;
+import org.mongeez.MongeezRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -36,9 +39,39 @@ public abstract class AbstractMongoConfig extends AbstractMongoConfiguration {
      */
     private Integer dabasePort;
 
+    /**
+     * The database user name
+     */
+    private String userName;
+
+    /**
+     * The database password
+     */
+    private String password;
+
+    /**
+     * Init.
+     */
     @PostConstruct
     public void init() {
         logger.info(String.format("Mongo Configuration [%s]: databaseName [%s], databasePort [%s], databaseURI [%s], ", this.getClass().getSimpleName(), this.getDatabaseName(), this.getDabasePort(), this.getDabaseURI()));
+    }
+
+    /**
+     * Get mongeez runner mongeez runner.
+     *
+     * @return the mongeez runner
+     */
+    @Bean
+    public MongeezRunner getMongeezRunner() throws Exception {
+        logger.info("MongeezRunner Running");
+        MongeezRunner mongeezRunner = new MongeezRunner();
+        mongeezRunner.setMongo(mongo());
+        mongeezRunner.setUserName(this.getUserName());
+        mongeezRunner.setPassWord(this.getPassword());
+        mongeezRunner.setFile(new FileSystemResource("classpath:mongeez/mongeez.xml"));
+        mongeezRunner.setExecuteEnabled(true);
+        return mongeezRunner;
     }
 
 
@@ -68,7 +101,7 @@ public abstract class AbstractMongoConfig extends AbstractMongoConfiguration {
     /**
      * the dabaseURI setter
      *
-     * @param dabaseURI
+     * @param dabaseURI the dabase uri
      */
     public void setDabaseURI(String dabaseURI) {
         this.dabaseURI = dabaseURI;
@@ -90,5 +123,41 @@ public abstract class AbstractMongoConfig extends AbstractMongoConfiguration {
      */
     public void setDabasePort(Integer dabasePort) {
         this.dabasePort = dabasePort;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
