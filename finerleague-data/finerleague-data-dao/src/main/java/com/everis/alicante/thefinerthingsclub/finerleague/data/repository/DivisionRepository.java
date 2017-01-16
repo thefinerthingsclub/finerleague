9d6ebc932aa7da7ff7f955f9960b4170b1b849f0
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2017-01-16 15:53:10 +0100

    Space removed from the DivisionRepository

--- finerleague-data/finerleague-data-dao/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/repository/DivisionRepository.java
+++ finerleague-data/finerleague-data-dao/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/data/repository/DivisionRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * The interface Division repository.
  */
 public interface DivisionRepository extends MongoRepository<Division, String> {
+
 }
