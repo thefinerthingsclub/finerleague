f7aeb8a76bc674dd2f6a641faa09686365644904
Pablo <pmiralle@everis.com>
2017-01-17 20:21:49 +0100

    generador token base 64

--- finerleague-core/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/core/manager/SessionManager.java
+++ finerleague-core/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/core/manager/SessionManager.java
@@ -1,10 +1,17 @@
 package com.everis.alicante.thefinerthingsclub.finerleague.core.manager;
 
-import com.everis.alicante.thefinerthingsclub.finerleague.data.entity.Division;
-import com.everis.alicante.thefinerthingsclub.finerleague.data.repository.DivisionRepository;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.everis.alicante.thefinerthingsclub.finerleague.data.entity.Division;
+import com.everis.alicante.thefinerthingsclub.finerleague.data.repository.DivisionRepository;
+
+// TODO: Auto-generated Javadoc
 /**
  * The type Session manager.
  */
@@ -45,8 +52,27 @@ public class SessionManager extends AbstractManager<DivisionRepository, Division
      * Gets session.
      *
      * @param token the token
+     * @return the session
      */
     public void getSession(final String token) {
 
     }
+    
+    /**
+     * Generate token.
+     *
+     * @param login the login
+     * @return the string
+     */
+    private String generateToken(final String login) {
+    	final SecureRandom random = new SecureRandom();
+    	final StringBuffer sb = new StringBuffer();
+    	
+    	sb.append(login);
+    	sb.append(new BigInteger(130, random).toString(32));
+    	byte[] toEncode = sb.toString().getBytes(StandardCharsets.UTF_8);
+    	
+    	return Base64.getEncoder().encodeToString(toEncode);
+    }
+
 }
