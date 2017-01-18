d8d3491d96e65ec9bcd70f2cb9fc4ba75f54e0ce
Enrique Mingorance <enrique.mingorance.cano@everis.com>
2017-01-18 13:54:47 +0100

    security config added

---
+++ finerleague-rest/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/rest/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.everis.alicante.thefinerthingsclub.finerleague.rest.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * The type Security config.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Init.
+     */
+    @PostConstruct
+    public void init() {
+        this.logger.info("SecurityConfig Initializing...");
+
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/documentation/**", "/v2/api-docs", "/swagger-resources/**", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/documentation/swagger-ui.html", "/webjars/**").permitAll()
+                .antMatchers("/session/login").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
