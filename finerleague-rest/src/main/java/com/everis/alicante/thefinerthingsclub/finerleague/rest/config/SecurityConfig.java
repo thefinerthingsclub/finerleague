a1a03aa61571161ae12e85e51c49af8fa8e4ad82
cbougeno <bousack@gmail.com>
2017-01-18 13:17:01 +0100

    changes in pom parent and pom rest add SecurityConfig.java

--- finerleague-rest/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/rest/config/SecurityConfig.java
+++ finerleague-rest/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/rest/config/SecurityConfig.java
@@ -5,17 +5,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * Created by cboug on 17/01/2017.
- */
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/session/login").permitAll()
                 .antMatchers("/**").authenticated();
