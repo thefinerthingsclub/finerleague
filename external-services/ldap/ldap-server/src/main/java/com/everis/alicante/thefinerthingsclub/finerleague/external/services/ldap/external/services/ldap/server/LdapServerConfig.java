d71c0c3cc009562e98ddfb9cc439bf57663c0810
Enrique Mingorance Cano <emingora@everis.com>
2017-01-23 14:57:24 +0100

    LDAP Server

--- external-services/ldap/ldap-server/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/external/services/ldap/external/services/ldap/server/LdapServerConfig.java
+++ external-services/ldap/ldap-server/src/main/java/com/everis/alicante/thefinerthingsclub/finerleague/external/services/ldap/external/services/ldap/server/LdapServerConfig.java
@@ -1,6 +1,10 @@
 package com.everis.alicante.thefinerthingsclub.finerleague.external.services.ldap.external.services.ldap.server;
 
 import com.everis.alicante.thefinerthingsclub.finerleague.common.domain.constants.FinerLeagueConfigurationConstants;
+import org.apache.directory.api.util.FileUtils;
+import org.apache.directory.server.core.DefaultDirectoryService;
+import org.apache.directory.server.ldap.LdapServer;
+import org.apache.directory.server.protocol.shared.transport.TcpTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -8,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 
 /**
  * The type Ldap server config.
@@ -24,6 +29,23 @@ public class LdapServerConfig {
     @PostConstruct
     private void startServer() throws Exception {
         logger.info("LdapServerConfig initializing...");
+        final File workingDirectory = new File(System.getProperty("java.io.tmpdir") + "/apacheds-test1");
+        FileUtils.deleteDirectory(workingDirectory);
+
+        DefaultDirectoryService directoryService = new DefaultDirectoryService();
+        directoryService.setShutdownHookEnabled(true);
+        directoryService.setAllowAnonymousAccess(true);
+
+        directoryService.getChangeLog().setEnabled(false);
+
+        directoryService.startup();
+
+        LdapServer ldapServer = new LdapServer();
+        ldapServer.setDirectoryService(directoryService);
+
+        TcpTransport ldapTransport = new TcpTransport(11111);
+        ldapServer.setTransports(ldapTransport);
+        ldapServer.start();
     }
 
     /**
