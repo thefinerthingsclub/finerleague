ad9bed21228e747c6588ff7dd25279b96ac56e13
Vicente <pouyio@gmail.com>
2017-03-06 20:59:36 +0100

    added shared folder with header and footer

--- finerleague-web/src/app/app.module.ts
+++ finerleague-web/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
