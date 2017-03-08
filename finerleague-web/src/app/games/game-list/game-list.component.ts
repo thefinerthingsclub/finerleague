7d04a4d7f663581c08e236ed7ea2117cbb7bad16
yduartep <layahi@gmail.com>
2017-03-08 19:33:24 +0100

    Adding spinner and font-awesome to base structure

--- finerleague-web/src/app/games/game-list/game-list.component.ts
+++ finerleague-web/src/app/games/game-list/game-list.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Game } from '../shared/game';
 import { GameService } from '../shared/game.service';
+import { SpinnerService } from '../../core/spinner/spinner.service';
 
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
@@ -14,19 +14,30 @@ import 'rxjs/add/operator/map'
   styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+  public isRequesting: boolean = false;
   title: string = 'List of Games';
   games: Game[];
   
   constructor(
     private service: GameService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private spinnerService: SpinnerService
   ) {}
 
   ngOnInit() {
+      this.spinnerService.show();
+      this.isRequesting = true;
       this.route.params
           .map((params: Params) => this.service.findAll())
-          .subscribe((games: Game[])=> this.games = games);
+          .subscribe((games: Game[])=> {
+              this.games = games;
+              
+              var that = this;
+              setTimeout(function(){
+                that.spinnerService.hide();
+              }, 3000);
+      });
   }
 
   onSelect(game: Game) {
