9e2aea24efa3df5cc0b68244845ccf6a258e8388
Vicente <pouyio@gmail.com>
2017-04-14 17:01:55 +0200

    create game

--- finerleague-web/src/app/games/game-create/game-create.component.ts
+++ finerleague-web/src/app/games/game-create/game-create.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Game } from '../shared/game';
+import { GameService } from '../shared/game.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ftc-game-create',
   templateUrl: './game-create.component.html',
   styleUrls: ['./game-create.component.css']
 })
-export class GameCreateComponent implements OnInit {
+export class GameCreateComponent {
 
-  constructor() { }
+  gameForm: FormGroup;
 
-  ngOnInit() {
+
+  constructor(private fb: FormBuilder, private gameService: GameService) {
+    this.createForm();
   }
+
+  createForm() {
+    this.gameForm = this.fb.group({
+      name: ['', Validators.required ]
+    });
+  }
+
+  saveGame() {
+    this.gameService.save(this.prepareGame()).subscribe();
+  }
+
+  prepareGame() {
+    let game: Game = new Game(
+      null,
+      this.gameForm.value.name,
+      'asd'
+      );
+    return game;
+  }
+
 }
